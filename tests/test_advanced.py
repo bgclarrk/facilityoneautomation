d21613accb63775717649478afc3eca74e6aa20f
Brian Clark <bgclarrk@Brians-MacBook-Pro.local>
2024-08-07 11:50:04 -0700

    improve setup files for automation project

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -2,6 +2,12 @@
 
 import unittest
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 from .context import sample
 
 
@@ -10,6 +16,12 @@ class AdvancedTestSuite(unittest.TestCase):
 
     def test_thoughts(self):
         self.assertIsNone(sample.hmm())
+
+    def test_selenium():
+        driver = webdriver.chrome()
+        driver.get("https://www.selenium.dev/")
+        assert driver.title == "Selenium"
+        driver.quit()
 
 
 if __name__ == "__main__":
